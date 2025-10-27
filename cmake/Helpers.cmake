8a8548b33a9d3ae920d23f185c4a3b3d30392225
bbalouki <bbalouki@outlook.com>
2025-10-27 16:05:29 +0000

    add address sanitizer and warnings library

--- cmake/Helpers.cmake
+++ cmake/Helpers.cmake
@@ -1,5 +1,13 @@
 cmake_minimum_required(VERSION 3.23)
 
+option(BUILD_TESTS "Add tests" ON)
+option(BUILD_BENCHMARKS "Add benchmark analisys" OFF)
+option(BUILD_EXAMPLES "Build some examples" OFF)
+
+set(PROJECT_ENV "DEV" CACHE STRING "Development environment")
+set_property(CACHE PROJECT_ENV PROPERTY STRINGS "DEV" "PROD")
+message(STATUS "Building for environment: ${PROJECT_ENV}")
+
 function(set_cxx_std target_name standard)
     message(STATUS
         "Setting C++${standard} standard for: ${target_name}"
@@ -8,9 +16,9 @@ function(set_cxx_std target_name standard)
 endfunction()
 
 add_library(warnings INTERFACE)
-if (MSVC)
-    target_compile_options(warnings INTERFACE /W4)
-else()
+if (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+     target_compile_options(warnings INTERFACE /W4 /permissive-)
+elseif (CMAKE_CXX_COMPILER_ID MATCHES "Clang|GNU|AppleClang")
     target_compile_options(warnings INTERFACE
         -Wall 
         -Wextra 
