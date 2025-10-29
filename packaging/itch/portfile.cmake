6fa78b6d61d338700e68469e61ce1953bd4cae96
bbalouki <bbalouki@outlook.com>
2025-10-29 08:05:15 +0000

    rename vcpkg to packaging

--- packaging/itch/portfile.cmake
+++ packaging/itch/portfile.cmake
@@ -1,19 +1,19 @@
-vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO bbalouki/itchcpp
     REF "v${VERSION}"
-    SHA512 49a268e83a11e35724a92ddf37de5dfbec775a7b29a54c47ea160416ecb4501a21d7622db8cef814a733f0e43b811add9e1196781dd248e376c96a6913b5b25d
+    SHA512 0
     HEAD_REF main
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        -DBUILD_TESTS=OFF
-        -DBUILD_BENCHMARKS=OFF
-        -DBUILD_EXAMPLES=OFF
+        -DITCH_BUILD_TESTS=OFF
+        -DITCH_BUILD_BENCHMARKS=OFF
+        -DITCH_BUILD_EXAMPLES=OFF
+        -DITCH_PROJECT_ENV=PROD
 )
 
 vcpkg_cmake_install()
@@ -33,6 +33,3 @@ file(REMOVE_RECURSE
 
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
-
-
-# vcpkg install --overlay-ports=./vcpkg itch
